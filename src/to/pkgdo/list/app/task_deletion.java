7d7d675f6e4d212fcfdc6dadc30d3bfa38b8f9e2
USER <USER@LAPTOP-R7074FHK>
2024-11-27 12:30:04 +0800

    new

--- src/to/pkgdo/list/app/task_deletion.java
+++ src/to/pkgdo/list/app/task_deletion.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class task_deletion {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println("=== Delete a Task ===");
         System.out.print("Enter the task number you want to delete: ");
-        
+        int taskNum = sc.nextInt();
     }
 }
