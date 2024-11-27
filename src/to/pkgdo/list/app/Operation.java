887f29cac53f21d9a4d9a03ef114effb3327143e
USER <USER@LAPTOP-R7074FHK>
2024-11-27 13:33:45 +0800

    new

--- src/to/pkgdo/list/app/Operation.java
+++ src/to/pkgdo/list/app/Operation.java
@@ -2,8 +2,12 @@ package to.pkgdo.list.app;
 
 import java.util.Scanner;
 
-public class task_deletion {
+public class Operation {
     public static void main(String[] args){
+        
+    }
+    
+    public static void task_deletion(){
         Scanner sc = new Scanner(System.in);
         
         System.out.println("=== Delete a Task ===");
--- src/to/pkgdo/list/app/ToDoListApp.java
+++ src/to/pkgdo/list/app/ToDoListApp.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package to.pkgdo.list.app;
 
-/**
- *
- * @author USER
- */
-public class ToDoListApp {
 
-    /**
-     * @param args the command line arguments
-     */
+public class ToDoListApp {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Operation t = new Operation();
+        t.task_deletion();
+        
     }
     
 }
