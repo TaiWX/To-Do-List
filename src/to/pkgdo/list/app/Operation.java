b81fae9f519bfedabdb242c4c5aba507a7399725
USER <USER@LAPTOP-R7074FHK>
2024-11-27 13:55:43 +0800

    new

--- src/to/pkgdo/list/app/Operation.java
+++ src/to/pkgdo/list/app/Operation.java
@@ -3,9 +3,6 @@ package to.pkgdo.list.app;
 import java.util.Scanner;
 
 public class Operation {
-    public static void main(String[] args){
-        
-    }
     
     public static void task_deletion(){
         Scanner sc = new Scanner(System.in);
@@ -13,5 +10,32 @@ public class Operation {
         System.out.println("=== Delete a Task ===");
         System.out.print("Enter the task number you want to delete: ");
         int taskNum = sc.nextInt();
+        
+    }
+    
+    public static void task_sorting(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("""
+                           === Sort Tasks ===
+                           Sort by:
+                           1. Due date (Ascending)
+                           2. Due date (Descending)
+                           3. Priority (Hoigh to Low)
+                           4. Prioroty (Low to High)
+                           """);
+        
+        System.out.print("> ");
+        int taskNum = sc.nextInt();
+        
+    }
+    
+    public static void task_searching(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("=== Search Tasks ===");
+        System.out.print("Enter a keyword to search by title or description: ");
+        String search = sc.nextLine();
+        
     }
 }
--- src/to/pkgdo/list/app/ToDoListApp.java
+++ src/to/pkgdo/list/app/ToDoListApp.java
@@ -4,7 +4,7 @@ package to.pkgdo.list.app;
 public class ToDoListApp {
     public static void main(String[] args) {
         Operation t = new Operation();
-        t.task_deletion();
+        t.task_sorting();
         
     }
     
